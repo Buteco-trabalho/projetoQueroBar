85604bb35e8190587fa1f397b4f30c03116aa938
GardenXR <ricardo.garden1@gmail.com>
2021-06-30 20:08:56 -0300

    Melhorias nas setas e acrescentei o js da denuncia

--- config/config_denuncia/denuncia.js
+++ config/config_denuncia/denuncia.js
@@ -1,3 +1,37 @@
-function apagar(){
-    document.getElementById('txtd').innerHTML = ''
+class Denuncia {
+    tipo;
+    mensagem;
+    constructor(tipo, mensagem){
+        this.tipo = tipo;
+        this.mensagem = mensagem;
+    }
+}
+
+function salvarDados(){
+        mensagem = document.getElementById('txtd').value
+        tipo = document.getElementById('denuncia').value
+        let denuncia = new Denuncia(tipo, mensagem);
+        armazenarDenuncia(denuncia);
+    }
+function armazenarDenuncia(denuncia){
+    let ArrayofDenuncias;
+        if(localStorage.getItem("denuncias") == null){
+        ArrayofDenuncias = {
+            denuncias : []
+        }
+    } else {
+        ArrayofDenuncias = JSON.parse(localStorage.getItem("denuncias"))
+    }
+    if(ArrayofDenuncias.id == null){
+        ArrayofDenuncias.id = 1;
+    }
+    else {
+        ArrayofDenuncias.id++;
+    }
+    denuncia._id = ArrayofDenuncias.id 
+    ArrayofDenuncias
+    .denuncias
+    .push(denuncia)
+    localStorage.setItem("denuncias", JSON.stringify(ArrayofDenuncias))
+
 }
