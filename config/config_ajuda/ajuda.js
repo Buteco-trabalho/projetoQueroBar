78f950c2792e7912ce6a84e5d6ca1e6975096d60
luan otavio <luanotaviocostasilva@gmail.com>
2021-07-02 14:30:18 -0300

    adicionei o js do ajuda

--- config/config_ajuda/ajuda.js
+++ config/config_ajuda/ajuda.js
@@ -1,3 +1,38 @@
-function apagar(){
-    document.getElementById('txtd').innerHTML = ''
+class Sugestao {
+  mensagem;
+  email;
+  constructor(mensagem, email) {
+    this.email = email;
+    this.mensagem = mensagem;
+  }
 }
+
+function salvarDados() {
+  let mensagem = document.getElementById("txtd").value;
+  let email = document.getElementById("Email").value;
+  let sugestao = new Sugestao(mensagem, email);
+  armazenarSugestao(sugestao);
+  window.location.reload()
+}
+function armazenarSugestao(sugestao) {
+  let ArrayofSugestoes;
+  if (localStorage.getItem("Sugestao") == null) {
+    ArrayofSugestoes = {
+      Sugestao: [],
+    };
+  } else {
+    ArrayofSugestoes = JSON.parse(localStorage.getItem("Sugestao"));
+  }
+  if (ArrayofSugestoes.id == null) {
+    ArrayofSugestoes.id = 1;
+  } else {
+    ArrayofSugestoes.id++;
+  }
+  sugestao._id = ArrayofSugestoes.id;
+  ArrayofSugestoes.Sugestao.push(sugestao);
+  localStorage.setItem("Sugestao", JSON.stringify(ArrayofSugestoes));
+}
+window.onload = () => {
+  let mensagem = document.getElementById("txtd").value = "";
+  let email = document.getElementById("Email").value = "";
+};
